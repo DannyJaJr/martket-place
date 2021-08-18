1da25a37bac26ed0e57036f1874ece1d634286a0
Daniel Lafontant <ldanieljajr@gmail.com>
2021-08-17 21:47:43 -0400

    added

---
+++ src/homepage.component.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div className="hoepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JAVA</h1>
+                        <span className="subtitle">Show Now</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JAVASCRIPT</h1>
+                        <span className="subtitle">Show Now</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">PYTHON</h1>
+                        <span className="subtitle">Show Now</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">AWS</h1>
+                        <span className="subtitle">Show Now</span>
+                    </div>
+                </div>
+
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">REACT</h1>
+                        <span className="subtitle">Show Now</span>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+
+export default HomePage;
